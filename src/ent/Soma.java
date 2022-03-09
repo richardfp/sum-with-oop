3b6143aa8ea5ac2fa5d10996eaeb7d94f16928d6
Richard Filipe <ri_chardfilipe@hotmail.com>
2022-03-09 09:41:53 -0300

    Adicionado os Métodos

--- src/ent/Soma.java
+++
@@ -1,5 +0,0 @@
-package ent;
-
-public class Soma {
-
-}
---
+++ src/somaDeDoisValores/Soma.java
@@ -0,0 +1,62 @@
+package somaDeDoisValores;
+
+import javax.swing.JOptionPane;
+
+public class Soma {
+	
+	// Atributos //
+	private int valor1;
+	private int valor2;
+	private int resultado;
+	
+	//Construtores//
+	public Soma() {		
+	}
+
+	public Soma(int valor1, int valor2, int resultado) {
+		this.valor1 = valor1;
+		this.valor2 = valor2;
+		this.resultado = resultado;
+	}
+	
+	// Getters e Setters//
+
+	public int getValor1() {
+		return valor1;
+	}
+
+	public void setValor1(int valor1) {
+		this.valor1 = valor1;
+	}
+
+	public int getValor2() {
+		return valor2;
+	}
+
+	public void setValor2(int valor2) {
+		this.valor2 = valor2;
+	}
+
+	public int getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(int resultado) {
+		this.resultado = resultado;
+	}
+	
+	//Métodos da Classe//
+	
+	public void somaValores() {
+		this.setResultado(this.getValor1() + this.getValor2());
+	
+	} 
+		
+	private void receberValoresEConverterParaInteiros() {
+		//receber os valores//
+		String valor1 = JOptionPane.showInputDialog("Digite o valor 1");
+		String valor2 = JOptionPane.showInputDialog("Digite o Valor 2");
+	}
+	
+
+}
